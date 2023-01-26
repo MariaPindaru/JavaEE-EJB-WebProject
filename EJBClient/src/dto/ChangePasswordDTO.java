e141e75e9e9c73c192862fc37aef5956078101b9
maria <mariajojo7@yahoo.com>
2023-01-26 22:31:32 +0200

    Refactor

--- EJBClient/src/dto/ChangePasswordDTO.java
+++ EJBClient/src/dto/ChangePasswordDTO.java
@@ -46,4 +46,43 @@ public class ChangePasswordDTO {
 		return "ChangePasswordDTO [username=" + username + ", oldPassword=" + oldPassword + ", newPassword="
 				+ newPassword + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((newPassword == null) ? 0 : newPassword.hashCode());
+		result = prime * result + ((oldPassword == null) ? 0 : oldPassword.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ChangePasswordDTO other = (ChangePasswordDTO) obj;
+		if (newPassword == null) {
+			if (other.newPassword != null)
+				return false;
+		} else if (!newPassword.equals(other.newPassword))
+			return false;
+		if (oldPassword == null) {
+			if (other.oldPassword != null)
+				return false;
+		} else if (!oldPassword.equals(other.oldPassword))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	
+	
 }
